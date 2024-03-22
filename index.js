e7e0933f2e478a08b0cdc2793257e371a4f43d42
LuciferDevV <vlad.mudrii.25@gmail.com>
2024-03-22 20:52:46 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,159 @@
+const fs = require('fs');
+
+class TransactionAnalyzer {
+    constructor(transactions) {
+        this.transactions = transactions;
+    }
+
+    addTransaction(transaction) {
+        this.transactions.push(transaction);
+    }
+
+    getAllTransactions() {
+        return this.transactions;
+    }
+
+    getUniqueTransactionTypes() {
+        const types = new Set();
+        this.transactions.forEach(transaction => {
+            types.add(transaction.transaction_type);
+        });
+        return Array.from(types);
+    }
+
+    calculateTotalAmount() {
+        return this.transactions.reduce((total, transaction) => {
+            return total + parseFloat(transaction.transaction_amount);
+        }, 0);
+    }
+
+    calculateTotalAmountByDate(year, month, day) {
+        let total = 0;
+        this.transactions.forEach(transaction => {
+            const transactionDate = new Date(transaction.transaction_date);
+            if ((!year || transactionDate.getFullYear() === year) &&
+                (!month || transactionDate.getMonth() + 1 === month) &&
+                (!day || transactionDate.getDate() === day)) {
+                total += parseFloat(transaction.transaction_amount);
+            }
+        });
+        return total;
+    }
+
+    getTransactionsByType(type) {
+        return this.transactions.filter(transaction => transaction.transaction_type === type);
+    }
+
+    getTransactionsInDateRange(startDate, endDate) {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        return this.transactions.filter(transaction => {
+            const transactionDate = new Date(transaction.transaction_date);
+            return transactionDate >= start && transactionDate <= end;
+        });
+    }
+
+    getTransactionsByMerchant(merchantName) {
+        return this.transactions.filter(transaction => transaction.merchant_name === merchantName);
+    }
+
+    calculateAverageTransactionAmount() {
+        const totalAmount = this.calculateTotalAmount();
+        return totalAmount / this.transactions.length;
+    }
+
+    getTransactionsByAmountRange(minAmount, maxAmount) {
+        return this.transactions.filter(transaction => {
+            const amount = parseFloat(transaction.transaction_amount);
+            return amount >= minAmount && amount <= maxAmount;
+        });
+    }
+
+    calculateTotalDebitAmount() {
+        return this.transactions.reduce((total, transaction) => {
+            if (transaction.transaction_type === 'debit') {
+                return total + parseFloat(transaction.transaction_amount);
+            }
+            return total;
+        }, 0);
+    }
+
+    findMostTransactionsMonth() {
+        const monthCounts = {};
+        this.transactions.forEach(transaction => {
+            const month = new Date(transaction.transaction_date).getMonth() + 1;
+            if (monthCounts[month]) {
+                monthCounts[month]++;
+            } else {
+                monthCounts[month] = 1;
+            }
+        });
+        let mostTransactionsMonth = 1;
+        let maxTransactions = 0;
+        for (const month in monthCounts) {
+            if (monthCounts[month] > maxTransactions) {
+                maxTransactions = monthCounts[month];
+                mostTransactionsMonth = month;
+            }
+        }
+        return mostTransactionsMonth;
+    }
+
+    findMostDebitTransactionMonth() {
+        const debitTransactions = this.getTransactionsByType('debit');
+        const monthCounts = {};
+        debitTransactions.forEach(transaction => {
+            const month = new Date(transaction.transaction_date).getMonth() + 1;
+            if (monthCounts[month]) {
+                monthCounts[month]++;
+            } else {
+                monthCounts[month] = 1;
+            }
+        });
+        let mostDebitMonth = 1;
+        let maxDebitTransactions = 0;
+        for (const month in monthCounts) {
+            if (monthCounts[month] > maxDebitTransactions) {
+                maxDebitTransactions = monthCounts[month];
+                mostDebitMonth = month;
+            }
+        }
+        return mostDebitMonth;
+    }
+
+    mostTransactionTypes() {
+        const debitCount = this.getTransactionsByType('debit').length;
+        const creditCount = this.getTransactionsByType('credit').length;
+        if (debitCount > creditCount) {
+            return 'debit';
+        } else if (creditCount > debitCount) {
+            return 'credit';
+        } else {
+            return 'equal';
+        }
+    }
+
+    getTransactionsBeforeDate(date) {
+        const endDate = new Date(date);
+        return this.transactions.filter(transaction => {
+            const transactionDate = new Date(transaction.transaction_date);
+            return transactionDate < endDate;
+        });
+    }
+
+    findTransactionById(id) {
+        return this.transactions.find(transaction => transaction.transaction_id === id);
+    }
+
+    mapTransactionDescriptions() {
+        return this.transactions.map(transaction => transaction.transaction_description);
+    }
+}
+
+// Load transactions from JSON file
+const transactionsData = fs.readFileSync('transactions.json');
+const transactions = JSON.parse(transactionsData);
+
+const analyzer = new TransactionAnalyzer(transactions);
+
+module.exports = analyzer;
